d113f56946430d7203a15a0db9e5ce73dee41a1e
Pranava S Balugari <stalin.pranava@gmail.com>
2022-06-27 22:28:08 +1000

    Initialize the project and modules

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/elitnomad/servicesale
+
+go 1.18
+
+require github.com/ardanlabs/conf v1.5.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ardanlabs/conf"
+)
+
+func main() {
+	conf.New()
+	fmt.Println("Garage sale API - improved version")
+}
